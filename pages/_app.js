553a7e633c78cd64bbe56733497efe9b78aad48f
Appsynic <dev.appsynic@gmail.com>
2025-10-11 04:34:30 -0200

    Celestifan site favicon update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 // filepath: c:\Users\Acer\Documents\GitHub\CelestiFan-landingpage\pages\_app.js
-import '../styles/globals.css';
+
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>CelestiFan</title>
+        <link rel="icon" href="/favicon.ico.PNG" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
