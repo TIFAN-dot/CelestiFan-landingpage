2da020ed8e9b6644a1ef271b2052b6557102dad7
CelestiFan <celestifan@gmail.com>
2025-11-17 01:19:18 +0000

    Fix navigation scroll issues - add ScrollToTop and scroll padding

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async"; // ADD THIS
+import { HelmetProvider } from "react-helmet-async";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop"; // ADD THIS
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Features from "./pages/Features";
@@ -14,17 +15,18 @@ import Blog from "./pages/Blog";
 import Connect from "./pages/Connect";
 import NotFound from "./pages/NotFound";
 import Celeste from "./pages/Celeste";
-import Quiz from "./pages/Quiz"; // ADD THIS
+import Quiz from "./pages/Quiz";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <HelmetProvider> {/* ADD THIS WRAPPER */}
+    <HelmetProvider>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop /> {/* ADD THIS - Automatically scrolls to top on route change */}
           <Navbar />
           <main className="pt-16">
             <Routes>
@@ -35,14 +37,14 @@ const App = () => (
               <Route path="/blog" element={<Blog />} />
               <Route path="/connect" element={<Connect />} />
               <Route path="/celeste" element={<Celeste />} />
-              <Route path="/quiz" element={<Quiz />} /> {/* ADD THIS */}
+              <Route path="/quiz" element={<Quiz />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
           <Footer />
         </BrowserRouter>
       </TooltipProvider>
-    </HelmetProvider> {/* CLOSE WRAPPER */}
+    </HelmetProvider>
   </QueryClientProvider>
 );
 
